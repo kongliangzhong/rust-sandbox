2510c8e703d52b10fb1175ed4c6103f4a396cd77
kongliangzhong <kongliangzhong@gmail.com>
2021-09-28 13:14:52 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+fn array_test() {
+    let vec1 = vec![1, 2, 3, 4];
+    println!("{:#?}", vec1);
+    let vec2 = &mut vec1[..3].to_vec()[..];
+    println!("{:?}", vec2);
+    let vec3 = &mut vec1[3..].to_vec()[..];
+    println!("{:?}", vec3);
+}
+
 fn main() {
-    println!("Hello, world!  ");
+    array_test();
 }
